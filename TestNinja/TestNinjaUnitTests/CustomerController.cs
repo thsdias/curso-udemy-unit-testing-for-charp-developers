c93bee1ea9099d7b9e06dd0793fc3bce27e88ddd
thsdias <thdias.84@gmail.com>
2024-10-10 20:39:59 -0300

    Section 3: Core Unit Testing Techniques

---
+++ TestNinja/TestNinjaUnitTests/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        /// <summary>
+        /// Testing the Return Type of Methods.
+        /// </summary>
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+    }
+}
---
+++ TestNinja/TestNinjaUnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        /// <summary>
+        /// Testing Void Methods.
+        /// </summary>
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            _logger.Log("Teste");
+
+            Assert.That(_logger.LastError, Is.EqualTo("Teste"));
+        }
+
+        /// <summary>
+        /// Testing Methods that Throw Exceptions.
+        /// </summary>
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        /// <summary>
+        /// Testing Methods that Raise an Event.
+        /// </summary>
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            _logger.ErrorLogged += (sender, args) => { id = args; };
+
+            _logger.Log("Event");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
